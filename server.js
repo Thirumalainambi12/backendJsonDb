896f9c44e1a9401872ab7a9e8ca164cff06896a0
Thirumalainambi12 <kthirumalainambi39@gmail.com>
2025-11-25 23:18:14 -0800

    first commit

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
-const jsonServer = require("json-server");
-const path = require("path");
+import jsonServer from "json-server";
 
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "anime.json")); // your JSON file
+const router = jsonServer.router("Allanime.json");  // your json file name
 const middlewares = jsonServer.defaults();
 
 const PORT = process.env.PORT || 4000;
 
 server.use(middlewares);
-server.use(jsonServer.bodyParser);
 server.use(router);
 
 server.listen(PORT, () => {
-  console.log(`🔥 JSON Server is running at http://localhost:${PORT}`);
+  console.log(`JSON Server is running on port ${PORT}`);
 });
